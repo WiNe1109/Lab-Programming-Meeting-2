4adf720fad47de65f61b00e7555de454cc4a070f
WiNe1109 <dwiawesome8@gmail.com>
2024-11-03 21:19:29 +0700

    Add files via upload

---
+++ meeting_7/Problem2/main (1).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fact(int x){
+    if (x == 0 || x == 1){
+    return 1;
+    }
+    else
+    return x * fact(x-1);
+}
+int main(){
+    int x;
+    cout <<"Enter X Factorial: ";
+    cin >>x;
+    
+    if(x<0){
+        cout <<"Negative Number is invalid in Factorial";
+    }
+    else{
+        cout <<"Factorial of " << x <<" is " << fact(x);
+        }
+}
