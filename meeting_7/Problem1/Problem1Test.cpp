aad5b0222cdc73a5b3dd113f15b0f56e50ef194b
WiNe1109 <dwiawesome8@gmail.com>
2024-11-13 19:52:24 +0700

    Update Problem1Test.cpp

--- meeting_7/Problem1/Problem1Test.cpp
+++ meeting_7/Problem1/Problem1Test.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+void add(float A, float B){
+    cout <<"A"<<" + "<<"B"<<" = "<< A + B;
+}
+void sub(float A, float B){
+    cout <<"A"<<" - "<<" B "<<" = "<< A - B;
+}
+int main(){
+    float A, B;
+    int operation;
+    A = 2000;
+    B = 1000;
+    operation = 1;
+    
+   
+     if (operation == 1){
+        add (A, B);
+    }
+    else if (operation == 2){
+        sub (A, B);
+    }
+    else 
+    cout <<"Operation Invalid";
+    cout <<endl;
+    cout <<"Test is succesful"<<endl;
+    return 0;
+    
+
+}
